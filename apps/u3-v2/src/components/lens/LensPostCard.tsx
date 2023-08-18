f6f78d4e1a65360876869f5ff8c300ef0bf46d93
shixuewen <friendlysxw@163.com>
2023-08-18 18:18:54 +0800

    feat: reaction: like mirror

--- apps/u3-v2/src/components/lens/LensPostCard.tsx
+++ apps/u3-v2/src/components/lens/LensPostCard.tsx
@@ -1,8 +1,58 @@
 import styled from 'styled-components'
 import { LensFeedPost } from '../../api/lens'
 import dayjs from 'dayjs'
+import {
+  Post,
+  ProfileOwnedByMe,
+  ReactionType,
+  useActiveProfile,
+  useCreateMirror,
+  useReaction,
+} from '@lens-protocol/react-web'
+import { useLensAuth } from '../../contexts/AppLensCtx'
+import { useCallback, useMemo } from 'react'
 
 export default function LensPostCard({ data }: { data: LensFeedPost }) {
+  const { isLogin, lensLogin } = useLensAuth()
+  const { data: activeProfile } = useActiveProfile()
+  const publisher = activeProfile as ProfileOwnedByMe
+
+  const publication = useMemo(
+    () => ({ __typename: 'Post', ...data }) as unknown as Post,
+    [data],
+  )
+
+  const { execute: createMirror, isPending: isPendingMirror } = useCreateMirror(
+    { publisher },
+  )
+
+  const {
+    addReaction,
+    removeReaction,
+    hasReaction,
+    isPending: isPendingReaction,
+  } = useReaction({
+    profileId: publisher?.id,
+  })
+
+  const toggleReactionUpvote = useCallback(async () => {
+    const hasReactionTypeUpvote = hasReaction({
+      reactionType: ReactionType.UPVOTE,
+      publication,
+    })
+    if (hasReactionTypeUpvote) {
+      await removeReaction({
+        reactionType: ReactionType.UPVOTE,
+        publication,
+      })
+    } else {
+      await addReaction({
+        reactionType: ReactionType.UPVOTE,
+        publication,
+      })
+    }
+  }, [publication, hasReaction, removeReaction, addReaction])
+
   return (
     <CastBox>
       <div key={data.id}>
@@ -13,13 +63,43 @@ export default function LensPostCard({ data }: { data: LensFeedPost }) {
         <small>{dayjs(data.timestamp).format()}</small>
         <div>
           {/* <button>comment</button> */}
-          <button>like</button>
-          <button>report</button>
+          <button
+            onClick={() => {
+              if (!isLogin) {
+                lensLogin()
+                return
+              }
+              toggleReactionUpvote()
+            }}
+          >
+            {isPendingReaction
+              ? 'liking...'
+              : hasReaction({
+                  reactionType: ReactionType.UPVOTE,
+                  publication,
+                })
+              ? 'unlike'
+              : 'like'}
+          </button>
+          <button
+            onClick={() => {
+              if (!isLogin) {
+                lensLogin()
+                return
+              }
+              createMirror({
+                publication,
+              })
+            }}
+          >
+            {isPendingMirror ? 'mirroring...' : 'mirror'}
+          </button>
         </div>
       </div>
     </CastBox>
   )
 }
+
 const CastBox = styled.div`
   display: flex;
   flex-direction: column;
--- apps/u3-v2/src/hooks/lens/useCreateLensPost.ts
+++ apps/u3-v2/src/hooks/lens/useCreateLensPost.ts
@@ -2,7 +2,6 @@ import { useCallback } from 'react'
 import {
   CollectPolicyType,
   ContentFocus,
-  MetadataUploadHandler,
   ProfileOwnedByMe,
   ReferencePolicyType,
   useActiveProfile,
