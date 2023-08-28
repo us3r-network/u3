0c087207122af6fa30e5a978e17977f73ef277ce
shixuewen <friendlysxw@163.com>
2023-08-28 17:37:21 +0800

    fix: useUpdateDispatcherConfig

--- apps/u3-v2/src/contexts/AppLensCtx.tsx
+++ apps/u3-v2/src/contexts/AppLensCtx.tsx
@@ -3,7 +3,9 @@ import {
   createContext,
   useCallback,
   useContext,
+  useEffect,
   useMemo,
+  useRef,
   useState,
 } from 'react'
 import {
@@ -14,18 +16,14 @@ import {
   useWalletLogout,
   useActiveProfile,
   production,
+  useUpdateDispatcherConfig,
+  ProfileOwnedByMe,
 } from '@lens-protocol/react-web'
 import { bindings as wagmiBindings } from '@lens-protocol/wagmi'
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { LENS_ENV } from '../constants/lens'
 import LensLoginModal from '../components/lens/LensLoginModal'
-import {
-  LensClient,
-  development as dev,
-  isRelayerResult,
-  production as prod,
-} from '@lens-protocol/client'
 
 interface LensAuthContextValue {
   isLogin: boolean
@@ -35,10 +33,6 @@ interface LensAuthContextValue {
   openLensLoginModal: boolean
   setOpenLensLoginModal: (open: boolean) => void
 }
-
-const lensClient = new LensClient({
-  environment: LENS_ENV === 'production' ? prod : dev,
-})
 
 export const LensAuthContext = createContext<LensAuthContextValue>({
   isLogin: false,
@@ -68,13 +62,27 @@ export function LensAuthProvider({ children }: PropsWithChildren) {
   const { execute: login, isPending: isLoginPending } = useWalletLogin()
   const { execute: lensLogout } = useWalletLogout()
   const { data: wallet, loading: walletLoading } = useActiveProfile()
-
   const { isConnected } = useAccount()
   const { disconnectAsync } = useDisconnect()
 
   const { connectAsync } = useConnect({
     connector: new InjectedConnector(),
   })
+
+  const { execute: updateDispatcher } = useUpdateDispatcherConfig({
+    profile: wallet as ProfileOwnedByMe,
+  })
+
+  const updatedDispatcherFirst = useRef(false)
+  useEffect(() => {
+    if (updatedDispatcherFirst.current) return
+    if (!wallet) return
+    if (!wallet?.dispatcher) {
+      updateDispatcher({ enabled: true }).finally(() => {
+        updatedDispatcherFirst.current = true
+      })
+    }
+  }, [wallet, updateDispatcher])
 
   const lensLogin = useCallback(async () => {
     if (isConnected) {
@@ -86,52 +94,9 @@ export function LensAuthProvider({ children }: PropsWithChildren) {
       const walletClient = await connector.getWalletClient()
       const address = walletClient.account.address
 
-      const res = await login({
+      await login({
         address,
       })
-      const activeProfile = (res as any)?.value
-
-      if (!activeProfile?.dispatcher) {
-        const challenge =
-          await lensClient.authentication.generateChallenge(address)
-        const signature = await (walletClient as any).signMessage({
-          message: challenge,
-        })
-        await lensClient.authentication.authenticate(address, signature)
-
-        const typedDataResult =
-          await lensClient.profile.createSetDispatcherTypedData({
-            profileId: activeProfile.id,
-          })
-
-        // typedDataResult is a Result object
-        const data = typedDataResult.unwrap()
-
-        // sign with the wallet
-        const signedTypedData = await (walletClient as any).signTypedData({
-          account: address,
-          domain: data.typedData.domain,
-          primaryType: 'SetDispatcherWithSig',
-          types: data.typedData.types,
-          message: data.typedData.value,
-        })
-        // broadcast
-        const broadcastResult = await lensClient.transaction.broadcast({
-          id: data.id,
-          signature: signedTypedData,
-        })
-
-        // broadcastResult is a Result object
-        const broadcastResultValue = broadcastResult.unwrap()
-        if (!isRelayerResult(broadcastResultValue)) {
-          console.log(`Something went wrong`, broadcastResultValue)
-          return
-        }
-
-        console.log(
-          `Transaction was successfuly broadcasted with txId ${broadcastResultValue.txId}`,
-        )
-      }
     }
   }, [isConnected, disconnectAsync, connectAsync, login])
 
