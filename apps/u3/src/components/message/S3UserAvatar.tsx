b6529d82b171c39a51a14c61040d9ec805640831
shixuewen <friendlysxw@163.com>
2023-09-06 17:48:14 +0800

    fix:message avatar loading

--- apps/u3/src/components/message/S3UserAvatar.tsx
+++ apps/u3/src/components/message/S3UserAvatar.tsx
@@ -1,27 +1,20 @@
 import { UserAvatar, UserAvatarProps } from '@us3r-network/profile';
 import styled from 'styled-components';
-import Loading from '../common/loading/Loading';
 
 export default function S3UserAvatar(props: UserAvatarProps) {
-  return (
-    <UserAvatarStyled {...props}>
-      {({ isLoading, avatarSrc }) =>
-        isLoading ? <LoadingStyled /> : <ImgStyled src={avatarSrc} />
-      }
-    </UserAvatarStyled>
-  );
+  return <UserAvatarStyled {...props} />;
 }
 const UserAvatarStyled = styled(UserAvatar)`
   width: 40px;
   height: 40px;
   border-radius: 50%;
   overflow: hidden;
-`;
-const LoadingStyled = styled(Loading)`
-  width: 100%;
-  height: 100%;
-`;
-const ImgStyled = styled.img`
-  width: 100%;
-  height: 100%;
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+  }
 `;
