220493a148974b55d5a9a78c40261d2345a598b1
ttang <ttang@ttang-MBP-2021.lan>
2023-10-26 16:02:25 +0800

    feat: pin farcaster channel

---
+++ apps/u3/src/components/social/PinChannelBtn.tsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import { useMemo, useState } from 'react';
+
+import { useFarcasterCtx } from 'src/contexts/FarcasterCtx';
+
+import { SocialButtonPrimary } from './button/SocialButton';
+import { Pin } from '../icons/pin';
+import { PinRed } from '../icons/pin-red';
+
+export default function PinChannelBtn({
+  parent_url,
+  bg,
+}: {
+  parent_url: string;
+  bg?: string;
+}) {
+  const [loading, setLoading] = useState(false);
+  const { joinChannel, userChannels, currFid } = useFarcasterCtx();
+  const joined = useMemo(() => {
+    const joinItem = userChannels.find((c) => c.parent_url === parent_url);
+    return !!joinItem;
+  }, [userChannels, parent_url]);
+
+  if (!currFid) return null;
+  return (
+    <JoinBtn
+      bg={bg}
+      onClick={async (e) => {
+        e.preventDefault();
+        if (joined) return;
+        setLoading(true);
+        await joinChannel(parent_url);
+        await new Promise((resolve) => {
+          setTimeout(resolve, 1000);
+        });
+        setLoading(false);
+      }}
+    >
+      {loading ? '...' : joined ? <PinRed /> : <Pin />}
+    </JoinBtn>
+  );
+}
+
+const JoinBtn = styled(SocialButtonPrimary)<{ bg?: string }>`
+  border-radius: 10px;
+  border: 1px solid #39424c;
+  background: #1b1e23;
+  width: 40px;
+  height: 40px;
+  padding: 0;
+  color: #fff;
+`;
