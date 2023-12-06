1e9900bf6a334b8c5fa2ffcebc4720f36d59c03d
bufan <bufan@hotmail.com>
2023-12-06 17:19:24 +0800

    change arxiv to readerview

--- apps/u3/src/hooks/news/useFeedLinks.ts
+++ apps/u3/src/hooks/news/useFeedLinks.ts
@@ -2,7 +2,7 @@
  * @Author: bufan bufan@hotmail.com
  * @Date: 2023-11-21 18:38:19
  * @LastEditors: bufan bufan@hotmail.com
- * @LastEditTime: 2023-12-05 17:42:16
+ * @LastEditTime: 2023-12-06 17:17:10
  * @FilePath: /u3/apps/u3/src/hooks/news/useLinks.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -99,6 +99,7 @@ const DOMAINS_DO_NOT_SUPPORT_IFRAME = [
   'substack.com',
   'github.com',
   'bountycaster.xyz',
+  'arxiv.org',
 ];
 function checkSupportIframe(url) {
   const domain = url.split('/')[2];
