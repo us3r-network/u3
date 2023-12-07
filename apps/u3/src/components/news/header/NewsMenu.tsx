6a99926025ba6ee65c45c67f69650ec0f333db75
bufan <bufan@hotmail.com>
2023-12-07 15:01:22 +0800

    change links group icon

--- apps/u3/src/components/news/header/NewsMenu.tsx
+++ apps/u3/src/components/news/header/NewsMenu.tsx
@@ -2,7 +2,7 @@
  * @Author: shixuewen friendlysxw@163.com
  * @Date: 2023-01-03 16:10:28
  * @LastEditors: bufan bufan@hotmail.com
- * @LastEditTime: 2023-12-06 17:18:30
+ * @LastEditTime: 2023-12-07 14:59:22
  * @Description: file description
  */
 import { useEffect, useRef, useState } from 'react';
@@ -19,6 +19,22 @@ import ZoraIcon from './icon/zora.png';
 import SpotifyIcon from './icon/spotify.png';
 import SubstackIcon from './icon/substack.png';
 import ParagraphIcon from './icon/paragraph.png';
+import AburraIcon from './icon/aburra.png';
+import ArxivIcon from './icon/arxiv.png';
+import BasepaintIcon from './icon/basepaint.png';
+import BountycasterIcon from './icon/bountycaster.png';
+import FarQuestIcon from './icon/farquest.png';
+import FlinkIcon from './icon/flink.png';
+import KiwistandIcon from './icon/kiwistand.png';
+import LaunchcasterIcon from './icon/launchcaster.png';
+import MintFunIcon from './icon/mintfun.png';
+import MirrorIcon from './icon/mirror.png';
+import TilesIcon from './icon/tiles.png';
+import UnlonelyIcon from './icon/unlonely.png';
+import VercelIcon from './icon/vercel.png';
+import WarpcastIcon from './icon/warpcast.png';
+import WeponderIcon from './icon/weponder.png';
+import WithfabricIcon from './icon/withfabric.png';
 
 export const LinkGroup = [
   {
@@ -86,112 +102,128 @@ export const LinkGroup = [
     value: `/news/links/mirror`,
     group: 'mirror',
     includeDomains: ['mirror.xyz'],
-    icon: 'https://mirror.xyz/icon/favicon-32x32.png',
+    // icon: 'https://mirror.xyz/icon/favicon-32x32.png',
+    icon: MirrorIcon,
   },
   {
     label: 'Aburra',
     value: `/news/links/aburra`,
     group: 'aburra',
     includeDomains: ['aburra.xyz'],
-    icon: 'https://aburra.xyz/favicon.ico',
+    // icon: 'https://aburra.xyz/favicon.ico',
+    icon: AburraIcon,
   },
   {
     label: 'Launchcaster',
     value: `/news/links/launchcaster`,
     group: 'launchcaster',
     includeDomains: ['launchcaster.xyz'],
-    icon: 'https://www.launchcaster.xyz/favicon.ico',
+    // icon: 'https://www.launchcaster.xyz/favicon.ico',
+    icon: LaunchcasterIcon,
   },
   {
     label: 'Kiwistand',
     value: `/news/links/kiwistand`,
     group: 'kiwistand',
     includeDomains: ['news.kiwistand.com'],
-    icon: 'https://news.kiwistand.com/favicon.ico',
+    // icon: 'https://news.kiwistand.com/favicon.ico',
+    icon: KiwistandIcon,
   },
   {
     label: 'Weponder',
     value: `/news/links/weponder`,
     group: 'weponder',
     includeDomains: ['weponder.io'],
-    icon: 'https://weponder.io/favicon.ico',
+    // icon: 'https://weponder.io/favicon.ico',
+    icon: WeponderIcon,
   },
   {
     label: 'Unlonely',
     value: `/news/links/unlonely`,
     group: 'unlonely',
     includeDomains: ['unlonely.app'],
-    icon: 'https://unlonely.app/images/favicon-32x32.png',
+    // icon: 'https://unlonely.app/images/favicon-32x32.png',
+    icon: UnlonelyIcon,
   },
   {
     label: 'Bountycaster',
     value: `/news/links/bountycaster`,
     group: 'bountycaster',
     includeDomains: ['bountycaster.xyz'],
-    icon: 'https://bountycaster.xyz/static/favicons/apple-touch-icon.png',
+    // icon: 'https://bountycaster.xyz/static/favicons/apple-touch-icon.png',
+    icon: BountycasterIcon,
   },
   {
     label: 'Tiles',
     value: `/news/links/tiles`,
     group: 'tiles',
     includeDomains: ['tiles.cool'],
-    icon: 'https://www.tiles.cool/favicon.ico',
+    // icon: 'https://www.tiles.cool/favicon.ico',
+    icon: TilesIcon,
   },
   {
     label: 'Far.quest',
     value: `/news/links/far.quest`,
     group: 'far.quest',
     includeDomains: ['far.quest'],
-    icon: 'https://far.quest/favicon.ico',
+    // icon: 'https://far.quest/favicon.ico',
+    icon: FarQuestIcon,
   },
   {
     label: 'Withfabric',
     value: `/news/links/withfabric`,
     group: 'withfabric',
     includeDomains: ['withfabric.xyz'],
-    icon: 'https://withfabric.xyz/favicon.ico',
+    // icon: 'https://withfabric.xyz/favicon.ico',
+    icon: WithfabricIcon,
   },
   {
     label: 'Arxiv',
     value: `/news/links/arxiv`,
     group: 'arxiv',
     includeDomains: ['arxiv.org'],
-    icon: 'https://arxiv.org/favicon.ico',
+    // icon: 'https://arxiv.org/favicon.ico',
+    icon: ArxivIcon,
   },
   {
     label: 'Basepaint',
     value: `/news/links/basepaint`,
     group: 'basepaint',
     includeDomains: ['basepaint.xyz'],
-    icon: 'https://basepaint.xyz/favicon.ico',
+    // icon: 'https://basepaint.xyz/favicon.ico',
+    icon: BasepaintIcon,
   },
   {
     label: 'Warpcast',
     value: `/news/links/warpcast`,
     group: 'warpcast',
     includeDomains: ['warpcast.com'],
-    icon: 'https://warpcast.com/favicon.ico',
+    // icon: 'https://warpcast.com/favicon.ico',
+    icon: WarpcastIcon,
   },
   {
     label: 'Flink',
     value: `/news/links/flink`,
     group: 'flink',
     includeDomains: ['flink.fyi'],
-    icon: 'https://flink.fyi/favicon.ico',
+    // icon: 'https://flink.fyi/favicon.ico',
+    icon: FlinkIcon,
   },
   {
     label: 'Mint.fun',
     value: `/news/links/mint.fun`,
     group: 'mint.fun',
     includeDomains: ['mint.fun'],
-    icon: 'https://mint.fun/favicon.ico',
+    // icon: 'https://mint.fun/favicon.ico',
+    icon: MintFunIcon,
   },
   {
     label: 'Vercel',
     value: `/news/links/vercel`,
     group: 'vercel',
     includeDomains: ['vercel.app'],
-    icon: 'https://vercel.com/favicon.ico',
+    // icon: 'https://vercel.com/favicon.ico',
+    icon: VercelIcon,
   },
   // {
   //   label: 'Events',
