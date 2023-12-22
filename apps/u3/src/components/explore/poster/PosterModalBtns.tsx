686f7bbda64ac99e5a67109e0130e149a02ec3e8
shixuewen <friendlysxw@163.com>
2023-12-22 18:41:23 +0800

    feat: poster share & ui

--- apps/u3/src/components/explore/poster/PosterModalBtns.tsx
+++ apps/u3/src/components/explore/poster/PosterModalBtns.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { ComponentPropsWithRef } from 'react';
 import { ButtonInfo, ButtonPrimary } from '../../common/button/ButtonBase';
 import { useFarcasterCtx } from '../../../contexts/social/FarcasterCtx';
 import { useGlobalModalsCtx } from '../../../contexts/shared/GlobalModalsCtx';
@@ -6,6 +7,13 @@ import { POSTER_SHARE_DOMAIN } from '../../../constants';
 import { SocialPlatform } from '../../../services/social/types';
 import useLogin from '../../../hooks/shared/useLogin';
 import { getMetaTitle } from '../../../utils/shared/html-meta';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import FarcasterIcon from '@/components/common/icons/FarcasterIcon';
+import { TwitterLine } from '@/components/common/icons/twitter';
+import LensIcon from '@/components/common/icons/LensIcon';
+import { tweetShare } from '@/utils/shared/twitter';
+import { useLensCtx } from '@/contexts/social/AppLensCtx';
 
 export default function PosterModalBtns({
   shareDisabled,
@@ -23,12 +31,24 @@ export default function PosterModalBtns({
     openFarcasterQR,
   } = useFarcasterCtx();
   const { openShareLinkModal } = useGlobalModalsCtx();
+
+  const { isLogin: isLoginLens, setOpenLensLoginModal } = useLensCtx();
+
+  const shareLinkModalData = {
+    shareLink: POSTER_SHARE_DOMAIN,
+    shareLinkDefaultText: '',
+    shareLinkEmbedTitle: getMetaTitle(),
+    shareLinkDefaultPlatform: SocialPlatform.Farcaster,
+    shareLinkEmbedImg: posterImg,
+    shareLinkDomain: POSTER_SHARE_DOMAIN,
+  };
   return (
     <Wrapper>
-      <CloseBtn onClick={onClose}>
+      <ButtonBase onClick={onClose}>
         <ChevronLeftIcon />
-      </CloseBtn>
-      <ShareBtn
+      </ButtonBase>
+      <ShareButton
+        className=""
         disabled={shareDisabled || !posterImg}
         onClick={() => {
           if (!isLoginU3) {
@@ -40,20 +60,70 @@ export default function PosterModalBtns({
             return;
           }
           openShareLinkModal({
-            shareLink: POSTER_SHARE_DOMAIN,
-            shareLinkDefaultText: '',
-            shareLinkEmbedTitle: getMetaTitle(),
+            ...shareLinkModalData,
             shareLinkDefaultPlatform: SocialPlatform.Farcaster,
-            shareLinkEmbedImg: posterImg,
-            shareLinkDomain: POSTER_SHARE_DOMAIN,
           });
         }}
       >
+        <FarcasterIcon className="w-[20px] h-[20px]" />
         Share to Farcaster
-      </ShareBtn>
+      </ShareButton>
+      <ShareButton
+        disabled={shareDisabled || !posterImg}
+        onClick={() => {
+          tweetShare('', shareLinkModalData.shareLink);
+        }}
+      >
+        <TwitterLine className="w-[20px] h-[20px]" />
+        Share to Twitter
+      </ShareButton>
+      <ShareButton
+        disabled={shareDisabled || !posterImg}
+        onClick={() => {
+          if (!isLoginU3) {
+            login();
+            return;
+          }
+          if (!isLoginLens) {
+            setOpenLensLoginModal(true);
+            return;
+          }
+          openShareLinkModal({
+            ...shareLinkModalData,
+            shareLinkDefaultPlatform: SocialPlatform.Lens,
+          });
+        }}
+      >
+        <LensIcon className="w-[20px] h-[20px]" />
+        Share to Lens
+      </ShareButton>
+      <ButtonBase
+        className="px-[24px] bg-[linear-gradient(90deg,_#CD62FF_0%,_#62AAFF_100%)]"
+        disabled
+      >
+        Free Mint
+      </ButtonBase>
     </Wrapper>
   );
 }
+
+function ButtonBase({ className, ...props }: ComponentPropsWithRef<'button'>) {
+  return (
+    <Button
+      className={cn(
+        'bg-white h-[48px] p-[12px] gap-[8px] rounded-[12px] text-black text-[16px] font-bold',
+        'hover:bg-white hover:text-black',
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function ShareButton({ className, ...props }: ComponentPropsWithRef<'button'>) {
+  return <ButtonBase className={cn('flex-1', className)} {...props} />;
+}
+
 function ChevronLeftIcon() {
   return (
     <svg
@@ -82,10 +152,4 @@ const CloseBtn = styled(ButtonInfo)`
   width: 50px;
   height: 48px;
   flex-grow: 0.1 !important;
-`;
-const ShareBtn = styled(ButtonPrimary)`
-  width: 120px;
-  height: 48px;
-  flex-grow: 1;
-  font-weight: 700;
 `;
