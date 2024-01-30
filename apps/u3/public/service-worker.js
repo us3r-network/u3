ea8182b8fa6e3a1a4fc088153f2c5b9f21e1bc93
shixuewen <friendlysxw@163.com>
2024-01-30 17:32:59 +0800

    feat: add service-worker.js

---
+++ apps/u3/public/service-worker.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+// listen for push event
+self.addEventListener('push', (event) => {
+  let { title, body, icon } = event.data.json();
+  if (!title) title = 'U3 - Your Web3 Gateway';
+  if (!body) return;
+  self.registration.showNotification(title, {
+    body,
+    icon: icon || `logo192.png`,
+  });
+});
