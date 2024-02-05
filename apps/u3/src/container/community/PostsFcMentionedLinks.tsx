3138d4a4563f39c8932e1fc3fbb3a625cdf61805
shixuewen <friendlysxw@163.com>
2024-02-05 18:24:56 +0800

    feat: add links note

--- apps/u3/src/container/community/PostsFcMentionedLinks.tsx
+++ apps/u3/src/container/community/PostsFcMentionedLinks.tsx
@@ -2,6 +2,9 @@ import { useOutletContext } from 'react-router-dom';
 
 export default function PostsFcMentionedLinks() {
   const communityContext = useOutletContext<any>();
+  // 可以在这个组件根据这个channelId查询，也可以在上层context里查询，数据从communityContext里传递下来
+  // 可参考MembersLayout 里的 TotalMembers 组件
+  const { channelId } = communityContext;
   return (
     <div className="w-full h-full overflow-auto">
       <div className="text-[#fff] text-[16px] mt-[20px] text-center">
