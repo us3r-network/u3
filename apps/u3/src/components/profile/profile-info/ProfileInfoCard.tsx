481b467f7613cb1ba2d6e3c166c94e2212d1f8b2
bufan <bufan@hotmail.com>
2024-03-05 10:30:59 +0800

    reconstruct profile info

--- apps/u3/src/components/profile/profile-info/ProfileInfoCard.tsx
+++ apps/u3/src/components/profile/profile-info/ProfileInfoCard.tsx
@@ -1,35 +1,32 @@
 import { useProfileState } from '@us3r-network/profile';
-import { StyledComponentPropsWithRef } from 'styled-components';
-import { useEffect, useMemo, useState } from 'react';
+import { ComponentPropsWithRef, useEffect, useMemo, useState } from 'react';
+import { Profile as U3Profile } from '@us3r-network/data-model';
 import useDid from '../../../hooks/profile/useDid';
 // import Loading from '../../common/loading/Loading';
-import U3ProfileInfoCardContainer from './U3ProfileInfoCardContainer';
-import PlatformProfileInfoCardContainer from './PlatformProfileInfoCardContainer';
+import usePlatformProfileInfoData from '../../../hooks/profile/usePlatformProfileInfoData';
+import useU3ProfileInfoData from '../../../hooks/profile/useU3ProfileInfoData';
 import { isDidPkh } from '../../../utils/shared/did';
 import { getProfileShareUrl } from '../../../utils/shared/share';
+import ProfileInfoCardLayout from './ProfileInfoCardLayout';
 // import UserTagsStyled from '../UserTagsStyled';
 // import UserWalletsStyled from '../UserWalletsStyled';
 
-interface ProfileInfoCardProps extends StyledComponentPropsWithRef<'div'> {
+interface ProfileInfoCardProps extends ComponentPropsWithRef<'div'> {
   isSelf?: boolean;
   identity: string;
   canNavigateToProfile?: boolean;
   onNavigateToProfileAfter?: () => void;
-  clickFollowing?: () => void;
-  clickFollowers?: () => void;
 }
 export default function ProfileInfoCard({
   identity,
   isSelf,
   canNavigateToProfile,
   onNavigateToProfileAfter,
-  clickFollowing,
-  clickFollowers,
   ...wrapperProps
 }: ProfileInfoCardProps) {
   const { did, loading: didLoading } = useDid(identity);
   const { getProfileWithDid } = useProfileState();
-  const [hasProfile, setHasProfile] = useState(false);
+  const [u3Profile, setU3Profile] = useState(null);
   const [hasProfileLoading, setHasProfileLoading] = useState(false);
   useEffect(() => {
     (async () => {
@@ -37,11 +34,11 @@ export default function ProfileInfoCard({
         setHasProfileLoading(true);
         const profile = await getProfileWithDid(did);
         if (profile) {
-          setHasProfile(true);
+          setU3Profile(profile);
         }
         setHasProfileLoading(false);
       } else {
-        setHasProfile(false);
+        setU3Profile(null);
       }
     })();
   }, [did]);
@@ -59,22 +56,17 @@ export default function ProfileInfoCard({
     //   </LoadingWrapper>
     // );
   }
-  if (did && hasProfile) {
+  if (did && u3Profile) {
     return (
-      <>
-        <U3ProfileInfoCardContainer
-          isSelf={isSelf}
-          did={did}
-          navigateToProfileUrl={navigateToProfileUrl}
-          onNavigateToProfileAfter={onNavigateToProfileAfter}
-          clickFollowing={clickFollowing}
-          clickFollowers={clickFollowers}
-          shareLink={shareLink}
-          {...wrapperProps}
-        />
-        {/* <UserWalletsStyled did={did} />
-        <UserTagsStyled did={did} /> */}
-      </>
+      <U3ProfileInfoCardContainer
+        isSelf={isSelf}
+        did={did}
+        u3Profile={u3Profile}
+        navigateToProfileUrl={navigateToProfileUrl}
+        onNavigateToProfileAfter={onNavigateToProfileAfter}
+        shareLink={shareLink}
+        {...wrapperProps}
+      />
     );
   }
   if (identity) {
@@ -84,12 +76,104 @@ export default function ProfileInfoCard({
         identity={identity}
         navigateToProfileUrl={navigateToProfileUrl}
         onNavigateToProfileAfter={onNavigateToProfileAfter}
-        clickFollowing={clickFollowing}
-        clickFollowers={clickFollowers}
         shareLink={shareLink}
         {...wrapperProps}
       />
     );
   }
   return null;
+}
+
+interface PlatformProfileInfoCardContainerProps
+  extends ComponentPropsWithRef<'div'> {
+  identity: string;
+  navigateToProfileUrl?: string;
+  onNavigateToProfileAfter?: () => void;
+  isSelf: boolean;
+  shareLink: string;
+}
+function PlatformProfileInfoCardContainer({
+  identity,
+  isSelf,
+  navigateToProfileUrl,
+  onNavigateToProfileAfter,
+  shareLink,
+  ...wrapperProps
+}: PlatformProfileInfoCardContainerProps) {
+  const {
+    fid,
+    lensProfiles,
+    recommendAddress,
+    platformAccounts,
+    postsCount,
+    followersCount,
+    followingCount,
+    bioLinkLoading,
+  } = usePlatformProfileInfoData({ identity });
+  return (
+    <ProfileInfoCardLayout
+      identity={identity}
+      isSelf={isSelf}
+      navigateToProfileUrl={navigateToProfileUrl}
+      onNavigateToProfileAfter={onNavigateToProfileAfter}
+      loading={bioLinkLoading}
+      address={recommendAddress}
+      platformAccounts={platformAccounts}
+      postsCount={postsCount}
+      followersCount={followersCount}
+      followingCount={followingCount}
+      lensProfiles={lensProfiles}
+      fid={Number(fid)}
+      shareLink={shareLink}
+      {...wrapperProps}
+    />
+  );
+}
+
+interface U3ProfileInfoCardContainerProps extends ComponentPropsWithRef<'div'> {
+  did: string;
+  u3Profile: U3Profile;
+  isSelf?: boolean;
+  navigateToProfileUrl?: string;
+  onNavigateToProfileAfter?: () => void;
+  shareLink: string;
+}
+function U3ProfileInfoCardContainer({
+  did,
+  u3Profile,
+  isSelf,
+  navigateToProfileUrl,
+  onNavigateToProfileAfter,
+  shareLink,
+  ...wrapperProps
+}: U3ProfileInfoCardContainerProps) {
+  const {
+    fid,
+    address,
+    lensProfiles,
+    platformAccounts,
+    postsCount,
+    followersCount,
+    followingCount,
+    bioLinkLoading,
+  } = useU3ProfileInfoData({ did, isSelf: !!isSelf });
+
+  return (
+    <ProfileInfoCardLayout
+      isSelf={isSelf}
+      navigateToProfileUrl={navigateToProfileUrl}
+      onNavigateToProfileAfter={onNavigateToProfileAfter}
+      loading={bioLinkLoading}
+      u3Profile={u3Profile}
+      address={address}
+      platformAccounts={platformAccounts}
+      postsCount={postsCount}
+      followersCount={followersCount}
+      followingCount={followingCount}
+      lensProfiles={lensProfiles}
+      fid={Number(fid)}
+      shareLink={shareLink}
+      {...wrapperProps}
+    />
+  );
 }
--- apps/u3/src/components/profile/profile-info/ProfileInfoCardLayout.tsx
+++ apps/u3/src/components/profile/profile-info/ProfileInfoCardLayout.tsx
@@ -1,10 +1,10 @@
-import { UserInfo } from '@us3r-network/profile';
-import styled, { StyledComponentPropsWithRef, css } from 'styled-components';
-import { Profile } from '@lens-protocol/react-web';
-
+import { Profile as LensProfile } from '@lens-protocol/react-web';
+import { Profile as U3Profile } from '@us3r-network/data-model';
 import { useSession } from '@us3r-network/auth-with-rainbowkit';
-import PlatformAccounts, { PlatformAccountsData } from './PlatformAccounts';
-import Loading from '../../common/loading/Loading';
+import { toast } from 'react-toastify';
+import { ComponentPropsWithRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PlatformAccountData } from './PlatformAccounts';
 import PlatformFollowCounts from './PlatformFollowCounts';
 import ProfileBtns from './ProfileBtns';
 import {
@@ -15,41 +15,44 @@ import { shortPubKey } from '../../../utils/shared/shortPubKey';
 import { getDefaultAvatarWithIdentity } from '../../../utils/profile/avatar';
 import { SocialPlatform } from '../../../services/social/types';
 import { MultiPlatformShareMenuBtn } from '../../shared/share/MultiPlatformShareMenuBtn';
+import { Copy } from '../../common/icons/copy';
+import { cn } from '@/lib/utils';
+import { getLensProfileExternalLinkWithHandle } from '@/utils/social/lens/getLensExternalLink';
+import LensIcon from '@/components/common/icons/LensIcon';
+import { getFarcasterProfileExternalLinkWithHandle } from '@/utils/social/farcaster/getFarcasterExternalLink';
+import FarcasterIcon from '@/components/common/icons/FarcasterIcon';
 
 const MY_PROFILE_SHARE_TITLE = 'View my profile in U3!';
 const getShareNewFriendProfileTitle = (name) => `New friend ${name} in U3!`;
 
-interface ProfileInfoCardLayoutProps
-  extends StyledComponentPropsWithRef<'div'> {
-  isU3Profile: boolean;
+interface ProfileInfoCardLayoutProps extends ComponentPropsWithRef<'div'> {
   navigateToProfileUrl?: string;
   onNavigateToProfileAfter?: () => void;
-  did?: string;
+  u3Profile?: U3Profile;
   address: string;
-  platformAccounts: PlatformAccountsData;
+  identity?: string;
+  platformAccounts: PlatformAccountData[];
+  postsCount: number;
   followersCount: number;
   followingCount: number;
-  lensProfiles: Profile[];
+  lensProfiles: LensProfile[];
   fid?: number;
-  clickFollowing?: () => void;
-  clickFollowers?: () => void;
   loading?: boolean;
   isSelf: boolean;
   shareLink: string;
 }
 export default function ProfileInfoCardLayout({
-  isU3Profile,
   navigateToProfileUrl,
   onNavigateToProfileAfter,
-  did,
+  u3Profile,
+  identity,
   address,
   platformAccounts,
+  postsCount,
   followersCount,
   followingCount,
   lensProfiles,
   fid,
-  clickFollowing,
-  clickFollowers,
   loading,
   isSelf,
   shareLink,
@@ -65,139 +68,43 @@ export default function ProfileInfoCardLayout({
   );
   const showFollowBtn = !!findLensAccount || !!findFarcasterAccount;
   const showMessageBtn = !!address;
-
-  if (loading) {
-    return null;
-    // return (
-    //   <LoadingWrapper {...wrapperProps}>
-    //     <Loading />
-    //   </LoadingWrapper>
-    // );
-  }
-
-  if (isU3Profile) {
-    return (
-      <U3ProfileCardWrapper
-        did={did}
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-        {...wrapperProps}
-      >
-        {({ isLoginUser, info }) => {
-          return (
-            <>
-              <HeaderWrapper>
-                <U3ProfileBasicInfo
-                  did={did}
-                  navigateToProfileUrl={navigateToProfileUrl}
-                  onNavigateToProfileAfter={onNavigateToProfileAfter}
-                />
-                <PostShareMenuBtn
-                  shareLink={shareLink}
-                  shareLinkDefaultText={
-                    isLoginUser
-                      ? MY_PROFILE_SHARE_TITLE
-                      : getShareNewFriendProfileTitle(
-                          info?.name && !info?.name?.startsWith('0x')
-                            ? info?.name
-                            : platformAccounts?.[0]?.name ||
-                                platformAccounts?.[0]?.handle ||
-                                address
-                        )
-                  }
-                  shareLinkEmbedTitle={'Profile'}
-                  popoverConfig={{ placement: 'bottom end', offset: 0 }}
-                />
-              </HeaderWrapper>
-
-              <PlatformAccounts
-                isSelf={isSelf}
-                data={platformAccounts}
-                isLoginUser={isLoginUser}
-              />
-
-              <UserInfo.Bio />
-
-              <PlatformFollowCounts
-                followersCount={followersCount}
-                followingCount={followingCount}
-                clickFollowers={clickFollowers}
-                clickFollowing={clickFollowing}
-              />
-
-              {session?.id && !isLoginUser && (
-                <ProfileBtns
-                  showFollowBtn={showFollowBtn}
-                  showMessageBtn={showMessageBtn}
-                  lensProfiles={lensProfiles}
-                  fid={fid}
-                  address={address}
-                />
-              )}
-            </>
-          );
-        }}
-      </U3ProfileCardWrapper>
-    );
-  }
+  const navigate = useNavigate();
+  const pathSuffix = identity ? `/${identity}` : '';
 
   if (!address) {
     return null;
   }
 
   return (
-    <PlatformProfileCardWrapper
+    <div
+      className="flex flex-col gap-4 p-6"
       onClick={(e) => {
         e.stopPropagation();
       }}
       {...wrapperProps}
     >
-      <HeaderWrapper>
-        <PlatformProfileBasicInfo
-          data={{
-            avatar:
-              platformAccounts?.[0]?.avatar ||
-              getDefaultAvatarWithIdentity(
-                address || String(platformAccounts?.[0]?.id) || ''
-              ),
-            name: platformAccounts?.[0]?.name || shortPubKey(address),
-            address,
-            identity: platformAccounts?.[0]?.id,
-          }}
-          navigateToProfileUrl={navigateToProfileUrl}
-          onNavigateToProfileAfter={onNavigateToProfileAfter}
-        />
-        {platformAccounts.length > 0 && (
-          <PostShareMenuBtn
-            shareLink={shareLink}
-            shareLinkDefaultText={getShareNewFriendProfileTitle(
-              platformAccounts?.[0]?.name ||
-                platformAccounts?.[0]?.handle ||
-                address
-            )}
-            shareLinkEmbedTitle={'Profile'}
-            popoverConfig={{ placement: 'bottom end', offset: 0 }}
-          />
-        )}
-      </HeaderWrapper>
-
-      <PlatformAccounts data={platformAccounts} isSelf={isSelf} />
-
-      <PlatformBio>
-        {platformAccounts.length > 0
-          ? platformAccounts?.[0]?.bio
-          : 'There is nothing here'}
-      </PlatformBio>
-
-      <PlatformFollowCounts
-        followersCount={followersCount}
-        followingCount={followingCount}
-        clickFollowers={clickFollowers}
-        clickFollowing={clickFollowing}
+      <PlatformProfileBasicInfo
+        data={{
+          avatar:
+            u3Profile?.avatar ||
+            platformAccounts?.[0]?.avatar ||
+            getDefaultAvatarWithIdentity(
+              address || String(platformAccounts?.[0]?.id) || ''
+            ),
+          name:
+            u3Profile?.name ||
+            platformAccounts?.[0]?.name ||
+            shortPubKey(address),
+          bio:
+            u3Profile?.bio ||
+            platformAccounts?.[0]?.bio ||
+            'There is nothing here',
+          identity: platformAccounts?.[0]?.id,
+        }}
+        navigateToProfileUrl={navigateToProfileUrl}
+        onNavigateToProfileAfter={onNavigateToProfileAfter}
       />
-
-      {session?.id && (
+      {session?.id && !isSelf && (
         <ProfileBtns
           showFollowBtn={showFollowBtn}
           showMessageBtn={showMessageBtn}
@@ -206,71 +113,120 @@ export default function ProfileInfoCardLayout({
           address={address}
         />
       )}
-    </PlatformProfileCardWrapper>
+      <PlatformFollowCounts
+        identity={identity}
+        postsCount={postsCount}
+        followersCount={followersCount}
+        followingCount={followingCount}
+      />
+      {address && (
+        <Wallets
+          address={address}
+          onClick={() => navigate(`/u${pathSuffix}?type=following`)}
+        />
+      )}
+      {platformAccounts?.length > 0 && (
+        <SocialAccounts accounts={platformAccounts} />
+      )}
+
+      {platformAccounts.length > 0 && (
+        <div className="absolute top-6 right-6">
+          <MultiPlatformShareMenuBtn
+            shareLink={shareLink}
+            shareLinkDefaultText={getShareNewFriendProfileTitle(
+              u3Profile?.name && !u3Profile?.name?.startsWith('0x')
+                ? u3Profile?.name
+                : platformAccounts?.[0]?.name ||
+                    platformAccounts?.[0]?.handle ||
+                    address
+            )}
+            shareLinkEmbedTitle={'Profile'}
+            popoverConfig={{ placement: 'bottom end', offset: 0 }}
+          />
+        </div>
+      )}
+    </div>
   );
 }
-const HeaderWrapper = styled.div`
-  display: flex;
-  gap: 10px;
-  justify-content: space-between;
-`;
-export const PostShareMenuBtn = styled(MultiPlatformShareMenuBtn)`
-  border: none;
-  padding: 0px;
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  background: none;
-  &:not(:disabled):hover {
-    border: none;
-    background-color: #14171a;
-  }
-  & > svg {
-    width: 12px;
-    height: 12px;
-    cursor: pointer;
-    path {
-      stroke: #ffffff;
-    }
-  }
-`;
-const BaseWrapperCss = css`
-  padding: 20px;
-  box-sizing: border-box;
-`;
-const LoadingWrapper = styled.div`
-  ${BaseWrapperCss}
-  height: 230px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 
-const BioCss = css`
-  color: var(--718096, #718096);
+function SocialAccounts({
+  accounts,
+  onClick,
+  className,
+}: ComponentPropsWithRef<'div'> & {
+  accounts: PlatformAccountData[];
+  onClick?: (path: string) => void;
+}) {
+  return (
+    <div
+      className={cn('flex items-center justify-between', className)}
+      onClick={onClick}
+    >
+      <p className="text-gray-500">Social Accounts</p>
+      <div className="flex items-center gap-1">
+        {accounts.map((account) => {
+          if (account.platform === SocialPlatform.Lens) {
+            return (
+              <a
+                key={account.id}
+                title={account.name || account.handle || String(account.id)}
+                href={getLensProfileExternalLinkWithHandle(
+                  account.handle || account.name
+                )}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <LensIcon width="20px" height="20px" />
+              </a>
+            );
+          }
+          if (account.platform === SocialPlatform.Farcaster) {
+            return (
+              <a
+                key={account.id}
+                title={account.name || account.handle || String(account.id)}
+                href={getFarcasterProfileExternalLinkWithHandle(
+                  account.handle || account.name
+                )}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FarcasterIcon width="20px" height="20px" />
+              </a>
+            );
+          }
+          return null;
+        })}
+      </div>
+    </div>
+  );
+}
 
-  /* Text/Body 16pt · 1rem */
-  font-family: Rubik;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 24px; /* 150% */
-  opacity: 0.8;
-
-  display: block;
-  margin-top: 20px;
-`;
-
-const U3ProfileCardWrapper = styled(UserInfo)`
-  ${BaseWrapperCss}
-
-  [data-state-element='Bio'] {
-    ${BioCss}
-  }
-`;
-const PlatformProfileCardWrapper = styled.div`
-  ${BaseWrapperCss}
-`;
-const PlatformBio = styled.span`
-  ${BioCss}
-`;
+function Wallets({
+  address,
+  onClick,
+  className,
+}: ComponentPropsWithRef<'div'> & {
+  address: string;
+  onClick: (path: string) => void;
+}) {
+  return (
+    <div
+      className={cn('flex items-center justify-between', className)}
+      onClick={onClick}
+    >
+      <p className="text-gray-500">Wallet Address</p>
+      <div
+        className="flex items-center gap-1"
+        onClick={() => {
+          navigator.clipboard.writeText(address).then(() => {
+            toast.success('Copied!');
+          });
+        }}
+      >
+        <span className="text-white">{shortPubKey(address)}</span>
+        <Copy />
+      </div>
+    </div>
+  );
+}
