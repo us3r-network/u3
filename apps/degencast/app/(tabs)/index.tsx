2f8f922c0d66f1e885fbbc71614a6a9e1139c5e6
bufan <bufan@hotmail.com>
2024-03-21 18:33:30 +0800

    add NativeWind

--- apps/degencast/app/(tabs)/index.tsx
+++ apps/degencast/app/(tabs)/index.tsx
@@ -1,34 +1,14 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 // import { Text, View } from '@/components/Themed';
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { verifyInstallation } from 'nativewind';
 
 export default function ExploreScreen() {
-  // Ensure to call inside a component, not globally
-  // verifyInstallation();
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Explore Page</Text>
-      <View style={styles.separator} />
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl">Explore Page</Text>
+      <View className="h-[1px] w-[80%] mt-8" />
       <EditScreenInfo path="app/(tabs)/explore.tsx" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
--- apps/degencast/app/_layout.tsx
+++ apps/degencast/app/_layout.tsx
@@ -4,8 +4,10 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-
 import { useColorScheme } from '@/components/useColorScheme';
+
+// Import global CSS file
+import "../global.css"
 
 export {
   // Catch any errors thrown by the Layout component.
