f0f8e17bd4b34298e3a661b268b185ab4e216b8a
bufan <bufan@hotmail.com>
2024-03-22 18:45:09 +0800

    add shadow deps

--- apps/degencast/app/(tabs)/profile.tsx
+++ apps/degencast/app/(tabs)/profile.tsx
@@ -1,9 +1,17 @@
+import { usePrivy } from '@privy-io/react-auth';
 import { Text, View } from 'react-native';
+import { Button } from '~/components/ui/button';
 
 export default function ProfileScreen() {
+  const { login, } = usePrivy();
   return (
-    <View>
-      <Text>Profile Page</Text>
+    <View className="h-full flex items-center justify-center">
+      <Button
+        className="py-3 px-6 text-white rounded-lg"
+        onPress={login}
+      >
+        Log in
+      </Button>
     </View>
   );
 }
