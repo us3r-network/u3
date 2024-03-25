54c6c88bc208129f8a2020b1c2b05626daff24a3
bufan <bufan@hotmail.com>
2024-03-25 10:34:52 +0800

    prettier fix

--- apps/degencast/app/(tabs)/index.tsx
+++ apps/degencast/app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
-import { View, Text } from 'react-native';
+import { View, Text } from 'react-native'
 
 export default function ExploreScreen() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl">Explore Page</Text>
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-xl'>Explore Page</Text>
     </View>
-  );
+  )
 }
--- apps/degencast/app/(tabs)/profile.tsx
+++ apps/degencast/app/(tabs)/profile.tsx
@@ -4,20 +4,20 @@ import UserInfo from '~/components/profile/user';
 import { Button } from '~/components/ui/button';
 
 export default function ProfileScreen() {
-  const { login, ready, authenticated} = usePrivy();
+  const { login, ready, authenticated } = usePrivy();
   return (
     <View className="h-full mt-12 flex items-center justify-center">
-      {ready && 
-      (!authenticated ?
-      <Button
-        className="py-3 px-6 text-white rounded-lg"
-        onPress={login}
-      >
-        Log in
-      </Button>
-      :
-      <UserInfo />
-      )}
+      {ready &&
+        (!authenticated ?
+          <Button
+            className="py-3 px-6 text-white rounded-lg"
+            onPress={login}
+          >
+            Log in
+          </Button>
+          :
+          <UserInfo />
+        )}
     </View>
   );
 }
